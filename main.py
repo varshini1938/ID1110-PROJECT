454586620b59762fb96c1d97dc03ccfc2e16a306
varshini1938 <168060508+varshini1938@users.noreply.github.com>
2024-04-29 23:34:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,4 +33,72 @@ def mailstaff(mail_id, msg):
     s.quit()
     print('Mail Sent to staff')
 
+# variable for looping for input
+resp = 1
+
+#updating the attendance to the excel sheet
+while resp == 1:
+    #subject numbers
+    print("1--->phy\n2--->math\n3--->mech\n4--->python")
+
+    # enter the corresponding number
+    t = int(input("enter subject :"))
+
+    # taking input of the number of people absent for that course
+    no_of_absentees = int(input('no.of.absentees :'))
+
+    #taking the roll no.'s of the students on leave 
+    if no_of_absentees > 1:
+        x = list(map(int, (input('roll nos :').split(','))))
+    else:
+        x = [int(input('roll no :'))]
+
+    # list to hold row of the student in the excel sheet
+    row_num = []
+
+    # list to hold the total number of leaves taken by a particular student
+    leaves = []
+
+    #updating the excel sheet
+    for n in x:
+        #students
+        for i in range(2, rows+2):
+            if t == 1:
+                if sheet.cell(row=i, column=1).value == n:
+                    #updating the number of leaves
+                    s = sheet.cell(row=i, column=3).value
+                    s = s + 1
+                    sheet.cell(row=i, column=3).value = s
+                    #saving the data
+                    savefile()
+                    leaves.append(s)
+                    row_num.append(i)
+            elif t == 2:
+                if sheet.cell(row=i, column=1).value == n:
+                    s = sheet.cell(row=i, column=4).value
+                    s = s + 1
+                    sheet.cell(row=i, column=4).value = s
+                    savefile()
+                    leaves.append(s)
+                    row_num.append(i)
+            elif t == 3:
+                if sheet.cell(row=i, column=1).value == n:
+                    s = sheet.cell(row=i, column=5).value
+                    s = s + 1
+                    sheet.cell(row=i, column=5).value = s
+                    savefile()
+                    leaves.append(s)
+                    row_num.append(i)
+            elif t == 4:
+                if sheet.cell(row=i, column=1).value == n:
+                    s = sheet.cell(row=i, column=6).value
+                    s = s + 1
+                    sheet.cell(row=i, column=6).value = s
+                    savefile()
+                    leaves.append(s)
+                    row_num.append(i)
+
+    check(leaves, row_num, t)
+    #taking the input if the user wants to check for another subject
+    resp = int(input('another subject ? 1---->yes 0--->no'))
 
